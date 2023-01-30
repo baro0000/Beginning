d01bde198bd6501ec677f1a671d2f9745fc3be75
Bartosz Kulinski <bartosz.kulinski1@gmail.com>
2023-01-30 12:19:48 +0100

    git push -u origin main

--- src/SchoolProgrammer/Main.java
+++ src/SchoolProgrammer/Main.java
@@ -7,19 +7,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //MainMenu programmer = new MainMenu();
-        AttendanceOfDay x = new AttendanceOfDay();
-        x.godzinaWejscia[0] = 12;
-        x.godzinaWejscia[1] = 40;
-        x.godzinaWejscia[2] = 00;
+        MainMenu programmer = new MainMenu();
 
-        x.godzinaWyjscia[0] = 13;
-        x.godzinaWyjscia[1] = 10;
-        x.godzinaWyjscia[2] = 50;
-
-        int[] wynik = x.calculateDayTimeOfAttendance();
-        for(int value: wynik){
-            System.out.print(value + " : ");
         }
     }
 }
